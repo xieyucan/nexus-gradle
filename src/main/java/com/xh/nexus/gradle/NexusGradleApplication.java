47ffca46be023534d35657e741d42f994d8a6566
Bruce <xiehui1956@gmail.com>
2021-04-24 21:23:00 +0800

    init

---
+++ src/main/java/com/xh/nexus/gradle/NexusGradleApplication.java
@@ -0,0 +1,13 @@
+package com.xh.nexus.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusGradleApplication.class, args);
+	}
+
+}
